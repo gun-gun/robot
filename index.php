9558c2d6fe28fcaed20523ec3bb350952c970b0d
gun-gun <52319868+gun-gun@users.noreply.github.com>
2019-06-28 23:36:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $messages,
+                'text' => 'messages',
             ];
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
