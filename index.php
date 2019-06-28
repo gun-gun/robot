ef814d28f65db97e048358f463fbef6021f1a946
gun-gun <52319868+gun-gun@users.noreply.github.com>
2019-06-28 23:35:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => [$messages]
+                'messages' => "test"
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
