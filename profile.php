b2ce6beea40ee68d616fd8a6dea81cce269399cc
gun-gun <52319868+gun-gun@users.noreply.github.com>
2019-07-18 11:30:27 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@
 $access_token = 'T5OMx/29Ak4yNFiQI1GT+xbcgKhPWwpldUX27QyPZv2PjPx/w4ulCRSYDGcXd6BJtt6sNTxRxJczCzaexq6JLuFg24+HMN+0HoN8tb23Xsx+pcQuZJOmJy4SV633uniNbhQhnq2y01r8A4etpsGzsQdB04t89/1O/w1cDnyilFU=';
 
 /*$userId = 'Uffa138efe037e6e889d0b0f4a871c005'; */
-$userId = '5cf159e79a3555452be8021ea7a0ef8d';
+$userId = 'U4864f0f85194a59b3f337b2685c2a43c';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
